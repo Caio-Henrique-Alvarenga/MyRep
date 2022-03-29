99e1439f5b642e963109084158ef9060dfef4804
Caio Henrique <90706334+Caio-Henrique-Alvarenga@users.noreply.github.com>
2022-03-29 09:58:28 -0300

    Add files via upload

--- AEDsII/TP02Q01.java
+++ AEDsII/TP02Q01.java
@@ -1,92 +1,178 @@
 import java.io.*;
-import java.util;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-class Filme{
-   private String Nome;
-   private String TituloOriginal;
-   private Date DataDeLancamento;
-   private int Duracao;
-   private String Genero;
-   private String IdiomaOriginal;
-   private String Situacao;
-   private float Orcamento;
-   private String[] PalavrasChave;
+import javax.xml.catalog.Catalog;
 
-   Filme(){
-       public String getNome(){
-           return Nome;
-       }
-       public void setNome(String Nome){
-           Nome+=this.Nome;
-       }
-       public String getTituloOriginal(){
-           return TituloOriginal;
-       }
-       public void setTituloOriginal(String TituloOriginal){
-           TituloOriginal=this.TituloOriginal;
-       }
-       public Date getDataDeLancamento(){
-           return DataDeLancamento;
-       }
-       public void setDataDeLancamento(Date DataDeLancamento){
-           DataDeLancamento=this.DataDeLancamento;
-       }
-       public int getDuracao(){
-           return Duracao;
-       }
-       public void setDuracao(int Duracao){
-           Duracao=this.Duracao;
-       }
-       public String getGenero(){
-           return Genero;
-       }
-       public void setGenero(String Genero){
-           Genero+=this.Genero;
-       }
-       public String getIdiomaOriginal(){
-           return IdiomaOriginal;
-       }
-       public void setIdiomaOriginal(String IdiomaOriginal){
-           IdiomaOriginal+=this.IdiomaOriginal;
-       }
-       public String getSituacao(){
-           return Situacao;
-       }
-       public void setSituacao(String Situacao){
-           Situacao+=this.Situacao;
-       }
-       public float getOrcamento(){
-           return OrcOrcamento
-       }
-       public void setOrcamento(float Orcamento){
-           Orcamento=this.Orcamento;
-       }
-       public String[] getPalavrasChave(){
-           return PalavrasChave;
-       }
-       public void setPalavrasChave(String[] PalavrasChave){
-           int tamanho = PalavrasChave.length;
-           this.PalavrasChave =  new String[size];
+class Filme {
+    private String Nome;
+    private String TituloOriginal;
+    private Date DataDeLancamento;
+    private int Duracao;
+    private String Genero;
+    private String IdiomaOriginal;
+    private String Situacao;
+    private float Orcamento;
+    private String[] PalavrasChave;
 
-           for(int i = 0; i < tamanho; i++){
-               PalavrasChave[i]=this.PalavrasChave[i];
-           }
-       }
-   }
-   public Filme clone(){
-       Filme f = new Filme();
-       f.setNome(this.Nome);
-       f.setTituloOriginal(this.TituloOriginal);
-       f.setDataDeLancamento(this.DataDeLancamento);
-       f.setDuracao(this.Duracao);
-       f.setGenero(this.Genero);
-       f.setIdiomaOriginal(this.IdiomaOriginal);
-       f.setSituacao(this.Situacao);
-       f.setOrcamento(this.Orcamento);
-       f.setPalavrasChave(this.PalavrasChave);
-       return f;
-   }
-   public void Imprimir(){
-       
-   }
+    public void ler(String arquivo) {
+        int inicio=0;
+        String nome = "";
+        Arq.openRead(arquivo);
+        for (int i=0;i<3;i++){
+            Arq.readLine();
+        }
+        String format =  Arq.readLine();
+        for(int i=0;i<format.length();i++){
+            if(format.charAt(i)=='>'){
+                inicio=i+1;
+                i = format.length();
+            }
+        }
+        while(inicio < format.length()){
+            nome+=format.charAt(inicio);
+            if(format.charAt(inicio+1) == ' ' && format.charAt(inicio+2) == '(') {
+                inicio = format.length();
+            }
+            inicio++;
+        }
+        this.Nome = nome;
+        String  data = "";
+        SimpleDateFormat sdp = new SimpleDateFormat("dd/MM/yyyy");
+        boolean flag=false;
+        while(!flag&&Arq.hasNext()){
+            String linha=Arq.readLine();
+            String aux;
+            if(linha.contains("<span class="+"\""+"release"+"\"")){
+                flag=true;
+                aux=Arq.readLine();
+                for(int j=0;j<aux.length();j++){
+                    if(aux.charAt(j)!=' '){
+                        data+=aux.charAt(j);
+                    }
+                }
+            }
+        }
+        try{
+            this.DataDeLancamento=sdp.parse(data);
+        }catch(Exception e){
+        }
+    }
+
+    Filme(String arquivo) {
+        this.ler(arquivo);
+    }
+
+    public Filme(){
+
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getTituloOriginal() {
+        return TituloOriginal;
+    }
+
+    public void setTituloOriginal(String TituloOriginal) {
+        this.TituloOriginal = TituloOriginal;
+    }
+
+    public Date getDataDeLancamento() {
+        return DataDeLancamento;
+    }
+
+    public void setDataDeLancamento(Date DataDeLancamento) {
+        this.DataDeLancamento = DataDeLancamento;
+    }
+
+    public int getDuracao() {
+        return Duracao;
+    }
+
+    public void setDuracao(int Duracao) {
+        this.Duracao = Duracao;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    public String getIdiomaOriginal() {
+        return IdiomaOriginal;
+    }
+
+    public void setIdiomaOriginal(String IdiomaOriginal) {
+        this.IdiomaOriginal = IdiomaOriginal;
+    }
+
+    public String getSituacao() {
+        return Situacao;
+    }
+
+    public void setSituacao(String Situacao) {
+        this.Situacao = Situacao;
+    }
+
+    public float getOrcamento() {
+        return Orcamento;
+    }
+
+    public void setOrcamento(float Orcamento) {
+        this.Orcamento = Orcamento;
+    }
+
+    public String[] getPalavrasChave() {
+        return PalavrasChave;
+    }
+
+    public void setPalavrasChave(String[] PalavrasChave) {
+        int tamanho = PalavrasChave.length;
+        this.PalavrasChave = new String[tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            this.PalavrasChave[i] = PalavrasChave[i];
+        }
+    }
+
+    public Filme clone() {
+        Filme f = new Filme();
+        f.setNome(this.Nome);
+        f.setTituloOriginal(this.TituloOriginal);
+        f.setDataDeLancamento(this.DataDeLancamento);
+        f.setDuracao(this.Duracao);
+        f.setGenero(this.Genero);
+        f.setIdiomaOriginal(this.IdiomaOriginal);
+        f.setSituacao(this.Situacao);
+        f.setOrcamento(this.Orcamento);
+        f.setPalavrasChave(this.PalavrasChave);
+        return f;
+    }
+
+    public void Imprimir() {
+        System.out.println(this.Nome + " " + this.TituloOriginal + " " + this.DataDeLancamento + " " + this.Duracao
+                + " " + this.Genero + " " + this.IdiomaOriginal + " " + this.Situacao + " " + this.Orcamento + " "
+                + Arrays.toString(this.PalavrasChave));
+    }
+}
+
+class TP02Q01 {
+    public static boolean isFim(String s) {
+        return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
+
+    public static void main(String[] args) {
+        String arquivo = "filmes/007_ Sem Tempo para Morrer.html";
+        Filme f = new Filme(arquivo);
+        f.Imprimir();
+    }
 }
